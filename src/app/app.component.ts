5060ca4116ae9c78ac201c01f8a978f03c62ffd8
Luiz <luizfranzon05@hotmail.com>
2025-01-29 21:26:37 -0300

    `add:` base project boilerplate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  title = 'reviserr';
-}
+export class AppComponent {}
